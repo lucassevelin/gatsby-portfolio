d8605ef04dcc25a8959ac16b1b20b1a9e4c00abf
Lucas Sevelin <lucas.sevelin@gmail.com>
2022-04-15 18:57:45 +0200

    Updates styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,37 +1,22 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import navbar from "/css/navbar.css"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `#383961`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
+  <header className={navbar.navbarHeader}>
+    <div className={navbar.navbarMainDiv}>
       <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `#f5f5f5`,
-            textDecoration: `none`,
-          }}
-        >
+        <Link className={navbar.whiteLink}
+          to="/">
           {siteTitle}
         </Link>
       </h1>
       <div>
         <ul>
-          <Link to="/">About me</Link>
-          <Link to="/">My work</Link>
-          <Link to="/">Contact</Link>
+          <Link className={navbar.whiteLink} to="/">About me</Link>
+          <Link className={navbar.whiteLink} to="/">My work</Link>
+          <Link className={navbar.whiteLink} to="/">Contact</Link>
         </ul>
       </div>
     </div>
