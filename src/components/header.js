f33bc90fd4db33f5774c2ae78971ea74a6c6546d
Lucas Sevelin <lucas.sevelin@gmail.com>
2022-04-15 19:08:12 +0200

    Moved folder

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
-import navbar from "./css/navbar.module.css"
+import navbar from "../css/navbar"
 
 const Header = ({ siteTitle }) => (
   <header className={navbar.navbarHeader}>
