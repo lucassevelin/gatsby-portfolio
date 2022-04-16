cf6a44d26279519795300e634eaf75c24e281ef9
Lucas Sevelin <lucas.sevelin@gmail.com>
2022-04-16 15:06:40 +0200

    Fixed margins

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,19 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
-import { navbarHeader, navbarMainDiv, whiteLink, navbarItemsDiv, navbarItem } from "../css/navbar.module.css"
+import { navbarHeader, navbarMainDiv, whiteLink, navbarItemsDiv, navbarItem, navbarItemsUl } from "../css/navbar.module.css"
 
 const Header = ({ siteTitle }) => (
   <header className={navbarHeader}>
     <div className={navbarMainDiv}>
-      <h1 style={{ margin: 0 }}>
+      <h1>
         <Link className={whiteLink}
           to="/">
           {siteTitle}
         </Link>
       </h1>
       <div className={navbarItemsDiv}>
-        <ul>
+        <ul classNme={navbarItemsUl}>
           <Link className={navbarItem} to="/">About me</Link>
           <Link className={navbarItem} to="/">My work</Link>
           <Link className={navbarItem} to="/">Contact</Link>
