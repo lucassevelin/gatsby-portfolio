41e9a0ae5b381eb80d36a7a5c9d47dcdc0b9d398
Lucas Sevelin <lucas.sevelin@gmail.com>
2022-04-17 12:16:37 +0200

    Started a local dev copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,11 +10,10 @@ const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <div className={landingDiv}>
-      <h1 class="white">Hi, my name is Lucas Sevelin</h1>
-      <h2 class="white">I am an engineering student with a passion for automation and seamless integrations</h2>
+      <h1>Hi, my name is Lucas Sevelin</h1>
+      <h2>I am an engineering student with a passion for automation and seamless integrations</h2>
     </div>
     <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
       <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
       <Link to="/using-dsg">Go to "Using DSG"</Link>
